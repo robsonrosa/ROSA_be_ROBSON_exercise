2897015b11dee1152eac890c864019f10da0d5b3
Robson Rosa <robsonrosa@outlook.com>
2023-02-04 00:46:25 -0300

    fix: replace method from post to get

--- src/main/java/com/ecore/roles/web/rest/UsersRestController.java
+++ src/main/java/com/ecore/roles/web/rest/UsersRestController.java
@@ -5,8 +5,8 @@ import com.ecore.roles.web.UsersApi;
 import com.ecore.roles.web.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,33 +15,30 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static com.ecore.roles.web.dto.UserDto.fromModel;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/v1/users")
+@RequestMapping(value = "/v1/users", produces = APPLICATION_JSON_VALUE)
 public class UsersRestController implements UsersApi {
 
     private final UsersService usersService;
 
     @Override
-    @PostMapping(
-            produces = {"application/json"})
+    @GetMapping(path = "/{userId}")
+    public ResponseEntity<UserDto> getUser(@PathVariable final UUID userId) {
+        return ResponseEntity
+                .status(200)
+                .body(fromModel(usersService.getUser(userId)));
+    }
+
+    @Override
+    @GetMapping
     public ResponseEntity<List<UserDto>> getUsers() {
         return ResponseEntity
                 .status(200)
                 .body(usersService.getUsers().stream()
                         .map(UserDto::fromModel)
                         .collect(Collectors.toList()));
-    }
-
-    @Override
-    @PostMapping(
-            path = "/{userId}",
-            produces = {"application/json"})
-    public ResponseEntity<UserDto> getUser(
-            @PathVariable UUID userId) {
-        return ResponseEntity
-                .status(200)
-                .body(fromModel(usersService.getUser(userId)));
     }
 }
